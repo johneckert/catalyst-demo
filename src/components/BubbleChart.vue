e10fddecbc01ad1d196c8d714b9aa01ebef85554
johneckert <johnteckert@gmail.com>
2018-08-23 08:44:15 -0400

    updated bublle chart component to use new jscatalyst event

--- src/components/BubbleChart.vue
+++ src/components/BubbleChart.vue
@@ -2,7 +2,7 @@
   <v-flex row>
         <v-card :height="height">
           <bubble-chart
-          @click.native="selectBubble"
+          @jsc_click="selectBubble"
             :dataModel='bubbleData'
             title='D3 Bubble Chart'
           ></bubble-chart>
@@ -22,13 +22,9 @@ export default {
     ...mapGetters(["bubbleData", "height"])
   },
   methods: {
-    selectBubble() {
-      let tooltip = document.getElementsByClassName("d3_visuals_tooltip")[0];
-      let label = null;
-      if (tooltip.getElementsByTagName("b")[0]) {
-        label = tooltip.getElementsByTagName("b")[0].innerHTML;
-      }
-      this.bc.postMessage(label);
+    selectBubble(data) {
+      console.log(data);
+      this.bc.postMessage(data.label);
     }
   },
   created() {
